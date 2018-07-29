8e3fc97c7dcb0a91044a8de5af770a37e9cd71cd
Tyrone <tyrone.saulon@hotmail.com>
2018-07-29 04:32:46 -0400

    Adding transition animations and background colors for seekbar readability.

--- app/src/main/java/com/tsaulon/colorchanger/PickColor.kt
+++ app/src/main/java/com/tsaulon/colorchanger/PickColor.kt
@@ -2,14 +2,18 @@ package com.tsaulon.colorchanger
 
 import android.app.Activity
 import android.graphics.Color
+import android.opengl.Visibility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_pick_color.*
 import android.widget.SeekBar
 
-//  TODO: Introduce OOP design
+//  TODO: Introduce touch gestures
 
 class PickColor : Activity() {  //  Change AppCompatActivity() to Activity to remove default activity header.
 
@@ -17,6 +21,7 @@ class PickColor : Activity() {  //  Change AppCompatActivity() to Activity to re
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pick_color)
 
+        var seekBarsVisible = true
         init()  //  Initialize seekbar ranges and UI
 
         seekBarR.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
@@ -42,6 +47,29 @@ class PickColor : Activity() {  //  Change AppCompatActivity() to Activity to re
                 updateUI()
             }
         })
+
+        viewBackground.setOnClickListener{
+
+            seekBarsVisible = !seekBarsVisible
+
+            if(seekBarsVisible){
+                val fadeIn = AnimationUtils.loadAnimation(this, R.anim.abc_fade_in)
+                seekBarR.startAnimation(fadeIn)
+                Handler().postDelayed({seekBarR.visibility = View.VISIBLE}, 50)
+                seekBarG.startAnimation(fadeIn)
+                Handler().postDelayed({seekBarG.visibility = View.VISIBLE}, 50)
+                seekBarB.startAnimation(fadeIn)
+                Handler().postDelayed({seekBarB.visibility = View.VISIBLE}, 50)
+            } else{
+                val fadeOut = AnimationUtils.loadAnimation(this, R.anim.abc_fade_out)
+                seekBarR.startAnimation(fadeOut)
+                Handler().postDelayed({seekBarR.visibility = View.INVISIBLE}, 50)
+                seekBarG.startAnimation(fadeOut)
+                Handler().postDelayed({seekBarG.visibility = View.INVISIBLE}, 50)
+                seekBarB.startAnimation(fadeOut)
+                Handler().postDelayed({seekBarB.visibility = View.INVISIBLE}, 50)
+            }
+        }
     }
 
     //  initialize UI
@@ -53,13 +81,16 @@ class PickColor : Activity() {  //  Change AppCompatActivity() to Activity to re
     }
 
     fun updateUI(){
-        updateBackground()
+        updateBackgrounds()
         updateAccents()
         updateText()
     }
 
-    fun updateBackground() {
+    fun updateBackgrounds() {
         viewBackground.setBackgroundColor(Color.rgb(seekBarR.progress, seekBarG.progress, seekBarB.progress))
+        seekBarR.setBackgroundColor(Color.argb(80, 0, 0, 0))
+        seekBarG.setBackgroundColor(Color.argb(80, 0 , 0, 0))
+        seekBarB.setBackgroundColor(Color.argb(80, 0, 0, 0))
     }
 
     fun updateAccents(){
